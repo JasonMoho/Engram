a5a83b07b24d33b2adf15d10186e92e255cbc004
Jason <jasonmohoney@gmail.com>
2026-05-19 21:18:09 -0400

    Find local uv during Memex-SR bootstrap

--- deployments/memex-sr/scripts/bootstrap_local.sh
+++ deployments/memex-sr/scripts/bootstrap_local.sh
@@ -66,6 +66,13 @@ have() {
   command -v "$1" >/dev/null 2>&1
 }
 
+if [[ -f "$HOME/.local/bin/env" ]]; then
+  # shellcheck disable=SC1091
+  source "$HOME/.local/bin/env"
+else
+  export PATH="$HOME/.local/bin:$PATH"
+fi
+
 install_uv() {
   if have curl; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
